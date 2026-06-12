02e718fefa7f0f10cc538d1dd807c265b4eaf80d
JesusFigueroa25 <61164069@pronabec.edu.pe>
2026-06-12 10:57:27 -0500

    fix: corrige grafico de usabilidad

--- PROYECTO/backend-seabot/app/controllers/reports_controller.py
+++ PROYECTO/backend-seabot/app/controllers/reports_controller.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.database.database import get_db
 from app.services import reports_service
+import os
+
 
 router = APIRouter(prefix="/admin/reports", tags=["Reportes Admin"])
 
